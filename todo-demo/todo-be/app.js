0358facda2f2ac68f2a8b11cadb189642c1eca95
suhyang1166 <yooyootea1166@gmail.com>
2024-05-27 01:10:42 +0900

    login be 완료

--- todo-demo/todo-be/app.js
+++ todo-demo/todo-be/app.js
@@ -4,22 +4,24 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 
+require("dotenv").config();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
-
-const mongoURL = `mongodb://localhost:27017/todo-demo`;
+const mongoURL = MONGODB_URI_PROD;
 
 mongoose
-    .connect(mongoURL, { useNewUrlParser: true })
-    .then(() => {
-        console.log("mongoose connected");
-    })
-    .catch((err) => {
-        console.log("DB connection fail", err);
-    });
+  .connect(mongoURL)
+  .then(() => {
+    console.log("mongoose connected");
+  })
+  .catch((err) => {
+    console.log("DB connection fail", err);
+  });
 
-app.listen(5000, () => {
-    console.log("server on 5000");
+app.listen(process.env.PORT || 5500, () => {
+  console.log("server on 5500");
 });
